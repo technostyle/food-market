d2aaf8f29f3af33aeea9fe7a8d4040600aaea688
danila <dm.perlov@jet.su>
2020-04-18 14:35:37 +0300

    prod mode

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
